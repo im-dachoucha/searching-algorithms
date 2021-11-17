0f23afa1f78c5bf91b1e7f09b6f9b2961b437b71
abderrahmane <abderrahamane7@gmail.com>
2021-11-17 01:02:08 +0100

    linear search

--- file.c
+++ file.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main(){
-  printf("hello world!");
+  int idx = -1, nbr, arr[] = {1, 2, 4, 7, 8, 9, 90, 123, 300}, size = sizeof(arr)/sizeof(arr[0]);
+  printf("enter a number to look up in the array : ");
+  scanf("%d", &nbr);
+  for(int i = 0; i < size; i++){
+    if(nbr == arr[i]) idx = i;
+  }
+  if(idx >= 0) printf("the number you entered (%d) exists at the index %d", nbr, idx);
+  else printf("the number you entered doesn't exist");
 }
